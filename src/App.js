25604b7fa42441b5afb3fb3a18c0541c8f3444ab
klmr ✌🏼 <klmr@MacBook-Pro-de-klmr.local>
2024-02-26 11:22:52 +0100

    component up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Burger from './components/Burger.js';
 
 function App(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/'  />
+                <Route path='/'  element={<Burger />}/>
             </Routes>
         </BrowserRouter>
     )
