fa3e45df6aa50c96fabbe26e482c926fee5a734d
klmr ✌🏼 <klmr@MacBook-Pro-de-klmr.local>
2024-02-27 00:00:32 +0100

    return

--- src/components/AddTips.js
+++ src/components/AddTips.js
@@ -1,6 +1,6 @@
 import Header from "./Header";
 import Input from "./Inputt";
-import Tips from "./Tips";
+// import Tips from "./Tips";
 import Button from "./Button";
 
 function AddTips({isDarkMode}){
@@ -11,9 +11,9 @@ function AddTips({isDarkMode}){
             <Input placeholder="Cerise" label="Nom de table" id="nameTable"  />
             <Input placeholder="3" label="Montant de pourboire" id="tips" />
             <div className="shortcut">
-                <Tips value="0.50" />
+                {/* <Tips value="0.50" />
                 <Tips value="1.00" />
-                <Tips value="2.00" />
+                <Tips value="2.00" /> */}
             </div>
             <Button value="Ajouter un pourboire" />
         </body>
--- src/components/Tips.js
+++
@@ -1,9 +0,0 @@
-import { Fragment } from 'react';
-
-function Tips ({value}) {
-    return <>
-        <button className="tips">{value}</button>
-    </>
-}
-
-export default Tips
